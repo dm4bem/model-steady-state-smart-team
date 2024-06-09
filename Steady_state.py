72c706fd799a7f48994aead39cd88df69aa34013
duboirom <166378464+duboirom@users.noreply.github.com>
2024-06-09 23:45:19 +0200

    Update Steady_state.py

--- Steady_state.py
+++ Steady_state.py
@@ -19,12 +19,3 @@ print('pièce 0 : ', theta[0], ', pièce 1 : ',theta[1],', pièce 2 : ', theta[2
 
 y = np.zeros(A.shape[1])
 y[0:2] = 1
-
-
-# ### Calcul step response ###
-# # State-space
-# C = np.zeros(A.shape[1])
-# TC = {'A':A, 'C':C, 'G':G, 'b':b, 'f':f, 'y':y}
-# [As, Bs, Cs, Ds, us] = dm4bem.tc2ss(A,G,b,f)
-
-# λ = np.linalg.eig(As)[0]
