a93faf9a8537a36c52a816f11aaaafce46fde252
AymericGestin <166367085+AymericGestin@users.noreply.github.com>
2024-06-12 14:18:44 +0200

    Add files via upload

--- Steady_state.py
+++ Steady_state.py
@@ -10,10 +10,26 @@ import matplotlib.pyplot as plt
 from modélisationV6 import *
 import dm4bem
 
-### Calcul  steady-state###
+### Calcul  steady-state using DAE###
+T0 = 20 #température extérieure
+Tin = 20 #température intérieure au début
+Ti_sp=20
+Φo = 200 # W/m2 average solar irradiation on the exterior wall
+Φi = 100 # W/m2 surfacic power inside the building due to wall temperature
+Qa = 100 # W Human activity in the bulding that brings power to the building
+Φa = Φo
+bss=np.zeros(43)
+bss[[0,1,7,35,37,40]]=T0
+bss[39]=Ti_sp
+fss=np.zeros(33)
 theta = np.linalg.inv(A.T @ np.diag(G) @ A) @ (A.T @ np.diag(G) @ bss + fss)
 q = np.diag(G) @ (-A @ theta + bss)
-print('pièce 0 : ', theta[0], ', pièce 1 : ',theta[1],', pièce 2 : ', theta[2])
+print('Temperature without flux \n''pièce 0 : ', theta[0], ', pièce 1 : ',theta[1],', pièce 2 : ', theta[2])
+fss[[3,8,10,16,30,32]]=Φo
+fss[[7,17,19,14,20,22,23,25,26]]=Φi
+fss[0]=Φa
+theta = np.linalg.inv(A.T @ np.diag(G) @ A) @ (A.T @ np.diag(G) @ bss + fss)
+q = np.diag(G) @ (-A @ theta + bss)
+print('Temperature with flux \n''pièce 0 : ', theta[0], ', pièce 1 : ',theta[1],', pièce 2 : ', theta[2])
 
-y = np.zeros(A.shape[1])
-y[0:2] = 1
+
